79d7572db5a09c974eec50748846e8b1e2790cd6
Huy Vo Quang <huyvqhe163784@fpt.edu.vn>
2022-09-20 17:02:03 +0700

    Ex4 and half of Ex5

---
+++ source/repos/LINQ_Exercises/Ex5/Program.cs
@@ -0,0 +1,48 @@
+﻿using Ex1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("LINQ Practice - Exercise 4\n");
+            Console.WriteLine("1. Method Syntax");
+            Console.WriteLine("2. Query Syntax");
+            Console.WriteLine("Any numbers that are not 1 and 2. Exit");
+            Console.WriteLine("\nChoose an option: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Method m = new Method();
+            Query q = new Query();
+
+            while (choice == 1 || choice == 2)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Method.Show();
+                        Console.WriteLine("\nChoose an option: ");
+                        choice = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Query.Show();
+                        Console.WriteLine("\nChoose an option: ");
+                        choice = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
