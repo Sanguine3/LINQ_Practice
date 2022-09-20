73c24c2bb3803ddf45ddffca463ce050d321ddad
Huy Vo Quang <huyvqhe163784@fpt.edu.vn>
2022-09-20 17:24:14 +0700

    Add UI to external class

--- source/repos/LINQ_Exercises/Ex1/Program.cs
+++ source/repos/LINQ_Exercises/Ex1/Program.cs
@@ -1,4 +1,5 @@
-﻿using Ex1_Method;
+﻿using Ex1.Models;
+using Ex2;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,38 +12,7 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("LINQ Practice - Exercise 1\n");
-            Console.WriteLine("1. Method Syntax");
-            Console.WriteLine("2. Query Syntax");
-            Console.WriteLine("Numbers that are neither 1 nor 2. Exit");
-            Console.WriteLine("\nChoose an option: ");
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            Method m = new Method();
-            Query q = new Query();
-
-            while (choice == 1 || choice == 2)
-            {
-                switch (choice)
-                {
-                    case 1:
-                        Method.Show();
-                        Console.WriteLine("\nChoose an option: ");
-                        choice = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 2:
-                        Query.Show();
-                        Console.WriteLine("\nChoose an option: ");
-                        choice = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 3:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        break;
-                }
-            }
+            Menu.showMenu();
         }
     }
 }
--- source/repos/LINQ_Exercises/Ex2/Program.cs
+++ source/repos/LINQ_Exercises/Ex2/Program.cs
@@ -11,38 +11,7 @@ namespace Ex2
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("LINQ Practice - Exercise 2\n");
-            Console.WriteLine("1. Method Syntax");
-            Console.WriteLine("2. Query Syntax");
-            Console.WriteLine("Numbers that are neither 1 nor 2. Exit");
-            Console.WriteLine("\nChoose an option: ");
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            Method m = new Method();
-            Query q = new Query();
-
-            while (choice == 1 || choice == 2)
-            {
-                switch (choice)
-                {
-                    case 1:
-                        Method.Show();
-                        Console.WriteLine("\nChoose an option: ");
-                        choice = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 2:
-                        Query.Show();
-                        Console.WriteLine("\nChoose an option: ");
-                        choice = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 3:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        break;
-                }
-            }
+            Menu.showMenu();
         }
     }
 }
